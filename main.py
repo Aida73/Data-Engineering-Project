766d54402bb3be5b6bed6225005f6c7a138f1205
Aida Sow <sowaida@ept.sn>
2023-07-17 03:46:57 +0000

    first part of data extraction with the indicators titles and values

---
+++ main.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import os
+from utils import *
+
+
+def extract():
+    """
+    Return a dataframe after extracting the indicators from each country
+
+    """
+    batch_size = 20  # Set batch size and initialize counters
+    _, countries_pages = getCountries()
+    current_batch = 0
+    indicators_titles_list = getIndicatorsTitles()
+    total_batches = (len(countries_pages)//20)//batch_size
+
+    # save data to dataframe
+    df = pd.DataFrame(columns=['Country'] + indicators_titles_list)
+
+    # Iterate over the sections in batches
+    for batch in range(total_batches + 1):
+        # Get the current batch of sections
+        batch_sections = countries_pages[current_batch:current_batch + batch_size]
+
+        # Iterate over the sections in the current batch
+        for page in batch_sections:
+            try:
+                getCountriesData(page)
+            except requests.exceptions.Timeout:
+                print(
+                    f"Request for {page} timed out. Skipping to the next page.")
+
+        current_batch += batch_size
+        # time.sleep(5)
+
+    # create the Dataframe
+    for country, values in countries_data.items():
+        # Create a dictionary to hold the row data
+        row_data = {'Country': country}
+
+        for i, value in enumerate(values):
+            title = indicators_titles_list[i] if i < len(
+                indicators_titles_list) else ''
+            row_data[title] = value
+
+        # Append the row data to the DataFrame
+        df = df.append(row_data, ignore_index=True)
+
+    # Save the DataFrame to a CSV file
+    file_path = os.path.join('data', 'indicators.csv')
+    df.to_csv(file_path, index=False)
+
+    return df.head()
+
+
+def transform(dataframe):
+    """
+    Perform transformation on the DataFrame.
+    """
+    pass
+
+
+def load(dataframe):
+    """
+    Load the DataFrame into the target destination.
+    """
+    pass
---
+++ utils.py
@@ -0,0 +1,87 @@
+from bs4 import BeautifulSoup
+import random
+import re
+import requests
+
+
+# Declare all needed variables here
+bm_url = "https://donnees.banquemondiale.org/pays"
+countries_data = {}
+# change randomly header for skipping scrappers limitations
+user_agents_list = [
+    'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
+]
+
+
+def getSoup(url):
+    page = requests.get(url=bm_url)
+    return BeautifulSoup(page.content, 'html.parser')
+
+
+def getCountryName(url):
+    regex_pattern = r"/pays/(\w+)"
+    matches = re.findall(regex_pattern, url)
+    if matches:
+        country_name = matches[0]
+        return country_name
+    return None
+
+
+def getCountries():
+    countries = []
+    pages = []
+    contries_soup = getSoup(bm_url)
+    sections = contries_soup.find_all("section", class_="nav-item")
+    for section in sections:
+        a_tags = section.find_all("a", attrs={"data-reactid": True})
+        countries.extend([country.text for country in a_tags])
+        pages.extend([bm_url[:-5]+page['href']
+                     for page in a_tags if page['href']])
+    return countries, pages
+
+
+def getIndicatorsTitles():
+    _, pages = getCountries()
+    data = requests.get(url=pages[0],
+                        headers={'User-Agent': random.choice(user_agents_list)})
+    linked_page_soup = BeautifulSoup(data.content, 'html.parser')
+    indicators_titles = []
+    for indicator in linked_page_soup.find_all("div", {'class': 'indicator-item__wrapper'}):
+        titles = indicator.find_all("div", {'class': 'indicator-item__title'})
+        indicators_titles.extend([title.text for title in titles])
+    return indicators_titles
+
+
+def getIndicatorsValues(content):
+    indicators_values = []
+    for indicator in content:
+        values = indicator.find_all(
+            "div", {"class": "indicator-item__data-info"})
+        empty_value = indicator.find(
+            "p", {"class": "indicator-item__data-info-empty"})
+
+        indicators_values.extend([value.text for value in values])
+
+        if empty_value:
+            indicators_values.append(empty_value.text)
+    return indicators_values
+
+
+def getCountriesData(page):
+    linked_page_response = requests.get(url=page,
+                                        headers={
+                                            'User-Agent': random.choice(user_agents_list)},
+                                        timeout=30)  # add timeout to skip networkConnectionError
+
+    # Parse the linked page's HTML content
+    linked_page_soup = BeautifulSoup(linked_page_response.text, "html.parser")
+    indicators = linked_page_soup.find_all(
+        "div", {'class': 'indicator-item__wrapper'})
+    indicators_values = getIndicatorsValues(indicators)
+    print(page)
+    print(len(indicators_values))
+    if len(indicators_values) > 0:
+        country_name = getCountryName(page)
+        countries_data[country_name] = indicators_values
